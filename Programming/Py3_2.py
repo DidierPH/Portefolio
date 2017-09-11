ebbe1c5fee90b04218383528bc0e14f85a11cd9b
DidierPH <31732372+DidierPH@users.noreply.github.com>
2017-09-11 12:09:25 +0200

    Add files via upload

---
+++ Programming/Py3_2.py
@@ -0,0 +1,4 @@
+Leeftijd = eval(input('vul je leeftijd in'))
+Paspoort =  input('Ja of nee')
+if  Leeftijd >= 18 and Paspoort  == 'ja':
+ print('je mag stemmen')
---
+++ Programming/Py3_3.py
@@ -0,0 +1,5 @@
+Leeftijd = eval(input('vul je leeftijd in'))
+Paspoort =  input('Ja of nee')
+if  Leeftijd >= 18 and Paspoort  == 'ja':
+ print('je mag stemmen')
+else : print('je mag niet stemmen')
