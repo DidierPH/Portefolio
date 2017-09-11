b5152e12f6b1f5d01b047c90adc3758123a9f465
DidierPH <31732372+DidierPH@users.noreply.github.com>
2017-09-11 11:57:34 +0200

    Add files via upload

---
+++ Programming/Py2_2.py
@@ -0,0 +1,7 @@
+CijferICOR =6
+CijferPROG =6
+CijferCSN=6
+
+gemiddelde=((CijferICOR+CijferPROG+CijferCSN)/3)
+beloning=gemiddelde*90
+print('Mijn cijfers (gemiddeld een %d) leveren een beloning van $ %d op!'%(gemiddelde,beloning))
---
+++ Programming/Py2_4.py
@@ -0,0 +1,6 @@
+Loonperuur = 4.0
+Werkuur = 32
+
+
+geld= (Loonperuur*32)
+print ('32 uur werken levert % Euro op!' %(geld))
