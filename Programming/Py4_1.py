14c2ccdf9aa36a8f9796ab0010acee49b381b14d
DidierPH <31732372+DidierPH@users.noreply.github.com>
2017-09-14 16:34:19 +0200

    Add files via upload

---
+++ Programming/Py4_1.py
@@ -0,0 +1,8 @@
+def som(getal1,getal2,getal3):
+ optelling =getal1+getal2+getal3
+ return optelling
+
+getal1 = eval(input("getal 1: "))
+getal2 = eval(input("getal 2: "))
+getal3 = eval(input("getal 3: "))
+print(som(getal1,getal2,getal3))
